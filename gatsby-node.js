ff9470edc751d488a5b8de35c704d433a5c86be2
ksmorano <ksmorano@gmail.com>
2019-11-16 08:46:18 +0800

    gatsby-node (update): comment out createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,36 @@
-const path = require(`path`);
+// const path = require(`path`);
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const response = await graphql(`
-    query {
-      allWordpressPost {
-        edges {
-          node {
-            id
-            slug
-            date
-            title
-            content
-          }
-        }
-      }
-    }
-  `);
-  const blog = path.resolve(`./src/templates/blog.js`);
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const response = await graphql(`
+//     query {
+//       allWordpressPost {
+//         edges {
+//           node {
+//             id
+//             slug
+//             date
+//             title
+//             content
+//           }
+//         }
+//       }
+//     }
+//   `);
+//   const blog = path.resolve(`./src/templates/blog.js`);
 
-  response.data.allWordpressPost.edges.forEach(edge => {
-    let { id, slug, date, title, content } = edge.node;
+//   response.data.allWordpressPost.edges.forEach(edge => {
+//     let { id, slug, date, title, content } = edge.node;
 
-    createPage({
-      path: `blog/${slug}`,
-      component: blog,
-      context: {
-        id: id,
-        date: date,
-        title: title,
-        content: content
-      }
-    });
-  });
-};
+//     createPage({
+//       path: `blog/${slug}`,
+//       component: blog,
+//       context: {
+//         id: id,
+//         date: date,
+//         title: title,
+//         content: content
+//       }
+//     });
+//   });
+// };
